18bcc994c9e9558f4502431fc97d211184b72cbb
New-hostname <92860067+New-hostname@users.noreply.github.com>
2021-11-11 14:01:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
+   
+   int n = 0, primo = 1, secondo = 1, somma = 0, i = 0;
+   
+   cin >> n;
+   
+   cout << primo << endl;
+   cout << secondo << endl;
+   
+   for(i = 0; i < n; i++)
+   {
+      somma = primo + secondo;
+      primo = secondo;
+      secondo = somma;
+      
+      cout << somma << endl;
+   }
    return 0;
 }
